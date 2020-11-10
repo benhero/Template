0f33d06e3ced9ed0f44bc76c07ebf66f922bc727
Benhero <benhero@126.com>
2020-11-10 19:43:58 +0800

    ConstraintLayout: 1. 添加页面滑动 2. 添加宽高比 3. 添加链 4. 添加Barrier

--- app/src/main/java/com/ben/template/function/ConstraintLayoutActivity.kt
+++ app/src/main/java/com/ben/template/function/ConstraintLayoutActivity.kt
@@ -3,6 +3,7 @@ package com.ben.template.function
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.ben.framework.util.setVisible
 import com.ben.template.R
 import kotlinx.android.synthetic.main.activity_constraint_layout.*
@@ -13,14 +14,35 @@ class ConstraintLayoutActivity : AppCompatActivity(), View.OnClickListener {
         setContentView(R.layout.activity_constraint_layout)
 
         textView5.setOnClickListener(this)
+        textView13.setOnClickListener(this)
+        textView21.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
         when (v) {
-            textView5 -> {
+            textView5, textView21 -> {
                 v?.setVisible(false)
             }
-
+            textView13 -> {
+                val layoutParams = v?.layoutParams as ConstraintLayout.LayoutParams
+                when (layoutParams.horizontalChainStyle) {
+                    ConstraintLayout.LayoutParams.CHAIN_SPREAD -> {
+                        layoutParams.horizontalChainStyle =
+                            ConstraintLayout.LayoutParams.CHAIN_SPREAD_INSIDE
+                        textView13.text = "spread_inside"
+                    }
+                    ConstraintLayout.LayoutParams.CHAIN_SPREAD_INSIDE -> {
+                        layoutParams.horizontalChainStyle =
+                            ConstraintLayout.LayoutParams.CHAIN_PACKED
+                        textView13.text = "packed"
+                    }
+                    ConstraintLayout.LayoutParams.CHAIN_PACKED -> {
+                        layoutParams.horizontalChainStyle =
+                            ConstraintLayout.LayoutParams.CHAIN_SPREAD
+                        textView13.text = "spread"
+                    }
+                }
+            }
             else -> {
             }
         }
