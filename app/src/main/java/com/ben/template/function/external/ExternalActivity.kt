bd7045c3e1efc4e71491c247c22f6dbcd953555e
Benhero <benhero@126.com>
2021-02-07 17:59:33 +0800

    应用外： 壁纸模块：添加权限申请以及应用状态的显示

--- app/src/main/java/com/ben/template/function/external/ExternalActivity.kt
+++ app/src/main/java/com/ben/template/function/external/ExternalActivity.kt
@@ -1,18 +1,22 @@
 package com.ben.template.function.external
 
+import android.Manifest
+import android.app.Activity
 import android.app.KeyguardManager
+import android.app.WallpaperManager
 import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.ben.framework.MainHandler
 import com.ben.template.R
 import com.ben.template.XApplication
-import com.ben.template.function.ShapeActivity
 import kotlinx.android.synthetic.main.activity_external.*
 
 /**
@@ -38,6 +42,7 @@ class ExternalActivity : AppCompatActivity(), View.OnClickListener {
         setContentView(R.layout.activity_external)
         button.setOnClickListener(this)
         btn_wallpaper.setOnClickListener(this)
+        check_wallpaper.isChecked = isLiveWallpaperRunning(this, packageName)
     }
 
     /**
@@ -66,15 +71,48 @@ class ExternalActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     private fun setLiveWallPaper() {
-        val intent = Intent()
-        intent.action = "android.service.wallpaper.CHANGE_LIVE_WALLPAPER"
-        intent.putExtra(
-            "android.service.wallpaper.extra.LIVE_WALLPAPER_COMPONENT", ComponentName(
-                packageName,
-                StaticWallpaper::class.java.getCanonicalName()
-            )
-        )
-        startActivity(intent)
+        startLiveWallpaperPreview(this, packageName, StaticWallpaper::class.java.canonicalName)
+    }
+
+    /**
+     * 去往某个动态壁纸的预览页面,那里可以设置壁纸
+     *
+     * @param packageName 动态壁纸的包名
+     * @param classFullName 动态壁纸service类的类全名
+     */
+    private fun startLiveWallpaperPreview(
+        activity: Activity, packageName: String, classFullName: String
+    ) {
+        val componentName = ComponentName(packageName, classFullName)
+        val intent = Intent(WallpaperManager.ACTION_CHANGE_LIVE_WALLPAPER)
+        intent.putExtra(WallpaperManager.EXTRA_LIVE_WALLPAPER_COMPONENT, componentName)
+        activity.startActivityForResult(intent, 124)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 124) {
+            check_wallpaper.isChecked = isLiveWallpaperRunning(this, packageName)
+        }
+    }
+
+    /**
+     * 判断一个动态壁纸是否已经在运行
+     *
+     * @param context :上下文
+     * @param targetPackageName :要判断的动态壁纸的包名
+     * @return
+     */
+    private fun isLiveWallpaperRunning(context: Context, targetPackageName: String): Boolean {
+        val wallpaperManager = WallpaperManager.getInstance(context) // 得到壁纸管理器
+        val wallpaperInfo = wallpaperManager.wallpaperInfo // 如果系统使用的壁纸是动态壁纸话则返回该动态壁纸的信息,否则会返回null
+        if (wallpaperInfo != null) { // 如果是动态壁纸,则得到该动态壁纸的包名,并与想知道的动态壁纸包名做比较
+            val currentLiveWallpaperPackageName = wallpaperInfo.packageName
+            if (currentLiveWallpaperPackageName == targetPackageName) {
+                return true
+            }
+        }
+        return false
     }
 
     override fun onClick(v: View?) {
@@ -86,7 +124,22 @@ class ExternalActivity : AppCompatActivity(), View.OnClickListener {
                 finish()
             }
             btn_wallpaper -> {
-                setLiveWallPaper()
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    requestPermissions(
+                        arrayOf(
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE
+                        ), 1
+                    )
+
+                    if (ContextCompat.checkSelfPermission(
+                            this,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE
+                        ) == PackageManager.PERMISSION_GRANTED
+                    ) {
+
+                        setLiveWallPaper()
+                    }
+                }
             }
             else -> {
             }
